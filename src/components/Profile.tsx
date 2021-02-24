44bcb0abf3975d38cfccfe28bf5bc498bd68bb88
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2021-02-23 22:25:50 -0300

    componente perfil + assents

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/58275652?s=460&u=d0e6003dcb493d1ff1017455897ce7b6607cb7e4&v=4" alt="Vinicius Botelho" />
+            <div>
+                <strong>Vinicius Botelho</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    ); 
+}
