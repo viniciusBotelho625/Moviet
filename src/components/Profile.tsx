36f4f9d2fff530e985a3f3af5d36d82661ac95f2
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2021-02-26 12:42:32 -0300

    Função de aumentar de level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/58275652?s=460&u=d0e6003dcb493d1ff1017455897ce7b6607cb7e4&v=4" alt="Vinicius Botelho" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Vinicius Botelho</strong>
                 <p>
                     <img src="icons/level.svg" alt="level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
